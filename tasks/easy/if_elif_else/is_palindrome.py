8a4d633a5572ab0ac10baaab10c3bc7006bf4917
Kirillchumnoy <rebilion@gmail.com>
2022-08-01 17:48:25 +0300

    done_is_palindrome.py

--- tasks/easy/if_elif_else/is_palindrome.py
+++ tasks/easy/if_elif_else/is_palindrome.py
@@ -25,7 +25,11 @@ def is_palindrome(check_str: str) -> bool:
     :return: True - палиндром, False - нет
     :rtype: bool
     """
-    result = None
+    revers_str = check_str[::-1]
+    if revers_str == check_str:
+        result = True
+    else:
+        result = False
     return result
 
 
